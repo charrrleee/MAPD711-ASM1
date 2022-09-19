38c211a01620d6d5d0c815f3af7f9890a87968d7
charrrleee <charlcpy@gmail.com>
2022-09-19 14:24:33 -0400

    add comment

--- app/src/main/java/com/example/assignment1/MainActivity.kt
+++ app/src/main/java/com/example/assignment1/MainActivity.kt
@@ -13,25 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        // get submit button
         val btn = findViewById<Button>(R.id.btn_submit)
 
-
-
+        //defining a button click event
         btn.setOnClickListener {
+            //java reference object for edit text control
             val name = findViewById<EditText>(R.id.et_name)
             val address = findViewById<EditText>(R.id.et_address)
             val profession = findViewById<EditText>(R.id.et_profession)
             val dreamJob = findViewById<EditText>(R.id.et_dream_job)
             val favouriteFood = findViewById<EditText>(R.id.et_favourite_food)
 
+            //creating an intent object using both first and second activities.
             val intent = Intent(this@MainActivity, MainActivity2::class.java)
 
+            //getting value from edit text and assign into intent object using key and values pairs
             intent.putExtra("name", name.text.toString())
             intent.putExtra("address", address.text.toString())
             intent.putExtra("profession", profession.text.toString())
             intent.putExtra("dreamJob", dreamJob.text.toString())
             intent.putExtra("favouriteFood", favouriteFood.text.toString())
+
+            // starting sub activity
             startActivity(intent)
 
         }
--- app/src/main/java/com/example/assignment1/MainActivity2.kt
+++ app/src/main/java/com/example/assignment1/MainActivity2.kt
@@ -10,19 +10,24 @@ class MainActivity2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+        //defining an intent object
         val intent = intent
+
+        //getting values from previous activity using intent object
         val nameMsg = intent.getStringExtra("name")
         val addressMsg = intent.getStringExtra("address")
         val professionMsg = intent.getStringExtra("profession")
         val dreamJobMsg = intent.getStringExtra("dreamJob")
         val favouriteFoodMsg = intent.getStringExtra("favouriteFood")
 
+        //defining a java object for text view control
         val name = findViewById<TextView>(R.id.tv_name_input)
         val address = findViewById<TextView>(R.id.tv_address_input)
         val profession = findViewById<TextView>(R.id.tv_profession_input)
         val dreamJob = findViewById<TextView>(R.id.tv_dream_job_input)
         val favouriteFood = findViewById<TextView>(R.id.tv_favourite_food_input)
 
+        //assigning message value to textview control
         name.text = nameMsg
         address.text = addressMsg
         profession.text = professionMsg
